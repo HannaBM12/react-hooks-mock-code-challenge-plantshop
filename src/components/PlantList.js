261c724d64d95d6aed4b2f0e0a46514f3d2f6a7b
HannaBM12 <Hannabmulugeta@gmail.com>
2021-05-02 13:33:11 -0400

    CoreDeliverables

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,20 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plantList, searchByName}) {
+  
+  const filteredPlants = plantList.filter(plant =>{
+      if(plant.name.toLowerCase().includes(searchByName.toLowerCase())){
+        return plant
+      }
+  })
+
+  const plantDetails = filteredPlants.map(plant => {
+    return <PlantCard key={plant.id} name= {plant.name} image={plant.image} price={plant.price} />  
+ })
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards"> {plantDetails} </ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const[plants, setPlants] = useState([])
+  const[isLoaded, setIsLoaded]= useState(false)
+  const[searchByName, setSearchByName] = useState("")
+
+  useEffect(()=>{
+      fetch('http://localhost:6001/plants')
+      .then(res => res.json())
+      .then(data => {
+        setPlants(data)
+        setIsLoaded(true)
+      })
+
+    }, []);
+
+  if(!isLoaded)return <h3>Loading ... </h3>
+
+  function handleNewPlant(plantObj){
+      setPlants([...plants, plantObj])
+  }
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onHandleSubmitForm={handleNewPlant}/>
+      <Search searchByName={searchByName} setSearchByName= {setSearchByName}/>
+      <PlantList plantList = {plants} searchByName={searchByName}/>
     </main>
   );
 }
